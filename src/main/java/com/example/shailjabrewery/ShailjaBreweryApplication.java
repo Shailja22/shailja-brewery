242674f9d0b9ca2783b695733deb71f83ca79d57
Raushan kumar <raushangnit@gmail.com>
2023-09-29 17:37:17 +0530

    Initial commit

---
+++ src/main/java/com/example/shailjabrewery/ShailjaBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.shailjabrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShailjaBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShailjaBreweryApplication.class, args);
+	}
+
+}
